743fdfb14e95fb9597a618f9c39e673925982f7f
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-10-27 20:37:27 -0500

    se crean nuevos comandos para el makefile

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     """
@@ -10,7 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)} # To autofill slug with title
+    prepopulated_fields = {'slug': ('title',)}  # To autofill slug with title
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
