e20e4b34ce0bd549380418c298ef37d7ddbe9bff
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-10-27 23:29:54 -0500

    Se crean views y templates para listado de posts y detalle de post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    """
+    Lista de posts
+    """
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    """
+    Un solo post
+    """
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
