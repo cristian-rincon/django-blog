73454678753b94f9bee029d52162608d32642a92
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-10-31 13:28:18 -0500

    Se agrega paginacion al blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
@@ -6,10 +7,20 @@ def post_list(request):
     """
     Lista de posts
     """
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts por página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Si la página no es un número entero, retorna la página 1
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Si la pagina está fuera de rango, entrega la última página de resultados
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
